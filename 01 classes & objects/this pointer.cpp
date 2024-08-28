6fe55e38a7362d202ec89b7806e9e3a34adeb486
Aditya Pourush <adityapourush11@outlook.com>
2024-08-28 11:28:04 +0530

    this pointer applied

--- 01 classes & objects/this pointer.cpp
+++ 01 classes & objects/this pointer.cpp
@@ -29,7 +29,7 @@ class User {
 
     // Constructor for public attributes
     User(string name, int age, string role) {
-        cout << "Making an object from class User" << endl;
+        // cout << "Making an object from class User" << endl;
         this->name = name;
         this->age = age;
         this->role = role;
@@ -49,6 +49,10 @@ class User {
     }
     void increaseAge() {
         age++;
+    }
+
+    int compareAge(User user){
+        return this->age > user.age;
     }
 };
 
@@ -105,24 +109,47 @@ class SmartDevice {
         cout << "Battery Level: " << batteryLevel << "%" << endl;
         cout << "Connected to Network: " << (isConnected ? "Yes" : "No") << endl;
     }
+
+    void compareBatteryLevel(SmartDevice device){
+        if(this->batteryLevel > device.batteryLevel){
+            cout << this->name + " have more battery" << endl;
+        }
+        else if(this->batteryLevel < device.batteryLevel){
+            cout << device.name + " have more battery" << endl;
+        }
+        else {
+            cout << "Both have same battery level" << endl;
+        }
+    }
 };
 
 int main() {
-    User Aditya("Aditya Raj Pourush", 20, "Admin");
-    Aditya.displayInfo();
-    Aditya.increaseAge();
-    Aditya.displayInfo();
-    cout << "Username: " << Aditya.getUsername() << endl;
-    Aditya.setUsername("adityapourush");
-    Aditya.setPassword("asdf1234");
-    cout << "Username: " << Aditya.getUsername() << endl;
-    cout << "Password: " << Aditya.getPassword() << endl;
+    User Aditya("Aditya Raj Pourush", 20, "Admin");  // User 1
 
-    SmartDevice Television("Samsung TV", "Samsung", "QN90B");
-    Television.turnOn();
-    Television.connectToNetwork();
-    Television.displayInfo();
-    Television.turnOff();
-    Television.displayInfo();
+    User Akshat("Akshat Raj Piyush", 18, "User");    // User 2
+
+    if(Aditya.compareAge(Akshat)){
+        cout << "Aditya is older that Akshat" << endl;
+    } else {
+        cout << "Aditya is younger than Akshat" << endl;
+    }
+
+    SmartDevice Television("Samsung TV", "Samsung", "QN90B");  // Device 1
+
+    SmartDevice AirConditioner("Voltas AC", "VOLTAS", "ACIT56");  // Device 2
+
+    Television.compareBatteryLevel(AirConditioner);
+
+    Television.batteryLevel = 90;
+    AirConditioner.batteryLevel = 80;
+
+    Television.compareBatteryLevel(AirConditioner);
+    
     return 0;
 }
+
+
+// Output
+// Aditya is older that Akshat
+// Both have same battery level
+// Samsung TV have more battery
---
+++ 01 classes & objects/this_pointer.java
@@ -0,0 +1,63 @@
+public class this_pointer {
+
+    public static class User{
+        String username;
+        String Password;
+    
+        public String name;
+        public int age;
+        public boolean is_admin;
+    
+        String getUsername(){
+            return username;
+        }
+        String getPassword(){
+            return Password;
+        }
+        void setUsername(String username){
+            this.username = username;
+        }
+        void setPassword(String password){
+            this.Password = password;
+        }
+    
+        public User(String name, int age, boolean is_admin){
+            this.name = name;
+            this.age = age;
+            this.is_admin = is_admin;
+        }
+    
+        void display_info(){
+            System.out.println(name + " " + age + " " + is_admin);
+        }
+    
+        void changePassword(String newPassword){
+            Password = newPassword;
+        }
+        void increaseAge(){
+            age += 1;
+        }
+
+        void compareAge(User user){
+            if(this.age > user.age){
+                System.out.println(user.name + " is younger " + this.name);
+            }
+            else if(this.age < user.age){
+                System.out.println(this.name + " is younger " + user.name);
+            }
+            else{
+                System.out.println(this.name + " and " + user.name + " are of same age.");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        User Aditya = new User("Aditya Raj", 20, true);
+
+        User Akshat = new User("Akshat", 18, false);
+
+        Aditya.compareAge(Akshat);
+    }
+}
+
+// output
+// Akshat is younger Aditya Raj
